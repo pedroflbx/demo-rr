5a349e948ce66ea1644e3295dc96c6d7795ea08e
omar bouden <omarbouden@getflowbox.com>
2022-09-08 10:55:12 +0200

    build phone frame

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@mui/material";
+import { Stack } from "@mui/system";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stack
+      sx={{
+        height: "100vh",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          borderRadius: 4,
+          border: "2px solid black",
+          height: 700,
+          width: 375,
+          px: 2.5,
+          pt: 6,
+          pb: 8,
+        }}
+      >
+        <Box height="100%" border="1px solid grey"></Box>
+      </Box>
+    </Stack>
   );
-}
+};
 
 export default App;
